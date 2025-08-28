7369e18e18636aef183736d7465f2d06c6d25711
Ratul Talukder <ratul.info24@gmail.com>
2025-08-28 07:58:28 +0600

    Update 1_struct.cpp

--- 1_struct.cpp
+++ 1_struct.cpp
@@ -18,16 +18,14 @@ int main() {
     cin >> s2.name >> s2.id >> s2.cgpa;
     cin >> s3.name >> s3.id >> s3.cgpa;
 
+    cout << "The Student with the highest CGPA is :\n";
     if (s1.cgpa > s2.cgpa && s1.cgpa > s3.cgpa) {
-        cout << "The Student with the highest CGPA is :\n";
         cout << "ID: " << s1.id << '\n' << "Name: " << s1.name;
     }
     else if (s2.cgpa > s1.cgpa && s2.cgpa > s3.cgpa) {
-        cout << "The Student with the highest CGPA is :\n";
         cout << "ID: " << s2.id << '\n' << "Name: " << s2.name;             
     }
     else {
-        cout << "The Student with the highest CGPA is :\n";
         cout << "ID: " << s3.id << '\n' << "Name: " << s3.name;
     }
 }
