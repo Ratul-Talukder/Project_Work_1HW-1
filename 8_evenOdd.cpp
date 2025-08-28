c36ca5f03836479f15fdaa86ebed6996c5aaa09c
Ratul Talukder <ratul.info24@gmail.com>
2025-08-28 07:39:28 +0600

    Add files via upload

---
+++ 8_evenOdd.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+void evenOdd(int n) {
+
+    if (n & 1) {
+        cout << "The number " << n << " is an Odd number";
+    }
+    else {
+        cout << "The number " << n << " is a Even number";
+    }
+}
+
+int main() {
+
+    int n; cin >> n;
+    evenOdd(n);
+
+
+    return 0;
+}
